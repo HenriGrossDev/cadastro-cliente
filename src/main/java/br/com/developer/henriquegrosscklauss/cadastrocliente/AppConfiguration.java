37464ad8d3ca8823f340a04122689391b3b59a2f
Henrique Grosscklauss De Souza Costa <henrique.costa@portaldedocumentos.com.br>
2022-04-06 15:56:05 -0300

    Adicionando properties de desenvolvimento e produção

--- src/main/java/br/com/developer/henriquegrosscklauss/cadastrocliente/AppConfiguration.java
+++ src/main/java/br/com/developer/henriquegrosscklauss/cadastrocliente/AppConfiguration.java
@@ -1,9 +1,18 @@
 package br.com.developer.henriquegrosscklauss.cadastrocliente;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class AppConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
